4e9cf5ca2fdb1ae6a8a96847d57b24e3134460ad
pratap360 <pratapparui0360@gmail.com>
2023-05-20 19:37:00 +0530

    homepage-edits

--- js/script.js
+++ js/script.js
@@ -48,26 +48,26 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 // Scroll sections active link
 const sections = document.querySelectorAll('section[id]');
 
-window.addEventListener('scroll', scrollActive);
+// window.addEventListener('scroll', scrollActive);
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+// function scrollActive(){
+//     const scrollY = window.pageYOffset
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50
-        sectionId = current.getAttribute('id')
+//     sections.forEach(current => {
+//         const sectionHeight = current.offsetHeight
+//         const sectionTop = current.offsetTop - 50
+//         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop +sectionHeight)
-        {
-            document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.add('active')
-        }
-        else
-        {
-            document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.remove('active')
-        }
-    })
-}
+//         if(scrollY > sectionTop && scrollY <= sectionTop +sectionHeight)
+//         {
+//             document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.add('active')
+//         }
+//         else
+//         {
+//             document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.remove('active')
+//         }
+//     })
+// }
 // ---------------- On menu click scroll to active link --------------------
 
 
