da601305a029fe5f199d8724455692cbf480beac
pratap360 <pratapparui0360@gmail.com>
2024-09-01 16:37:25 +0530

    added custom cursor

--- js/script.js
+++ js/script.js
@@ -258,10 +258,29 @@ function DarkLightMode() {
 
 
 
+// ! custom cursor 
 
+const cursorDot = document.querySelector("[data-cursor-dot]") ;
+const cursorOutline = document.querySelector("[data-cursor-outline]") ;
 
+window.addEventListener("mousemove",function (e) {
+  const posX = e.clientX;
+  const posY = e.clientY;
 
+  cursorDot.style.left = `${posX}px`
+  cursorDot.style.top = `${posY}px`
 
+  // * with out animation 
+  // cursorOutline.style.left = `${posX}px`
+  // cursorOutline.style.top = `${posY}px`
+
+  // * with animation 
+  cursorOutline.animate({
+    left:`${posX}px`,
+    top: `${posY}px`
+  },{duration: 300, fill: "forwards"});
+
+})
 
 
 
