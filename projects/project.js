eee1ac7bbb237151e33ba0f51a1b57a73d134e6f
pratap360 <pratapparui0360@gmail.com>
2024-09-15 18:09:05 +0530

    mouse cursor effect update

--- projects/project.js
+++ projects/project.js
@@ -41,6 +41,32 @@ function DarkLightMode() {
   });
 }
 
+// ! custom cursor 
+
+const cursorDot = document.querySelector("[data-cursor-dot]") ;
+const cursorOutline = document.querySelector("[data-cursor-outline]") ;
+
+window.addEventListener("mousemove",function (e) {
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`
+  cursorDot.style.top = `${posY}px`
+
+  // * with out animation 
+  // cursorOutline.style.left = `${posX}px`
+  // cursorOutline.style.top = `${posY}px`
+
+  // * with animation 
+  cursorOutline.animate({
+    left:`${posX}px`,
+    top: `${posY}px`
+  },{duration: 300, fill: "forwards"});
+
+})
+
+
+
 // Call the DarkLightMode function at the end of your script
 DarkLightMode();
 
